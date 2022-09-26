e9c7d2e712de90d8847bda93d71d67c54cc3084e
FriedColonel <huytran582002@gmail.com>
2022-09-26 20:28:08 +0700

    Moduling user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :config_devise_params, if: :devise_controller?
+
+  private
+  def config_devise_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username])
+  end
 end
